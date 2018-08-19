86c20ab1cdf3da49d0593aeceeafd4f174883d45
samlazrak <samlazrak@outlook.com>
2018-08-19 17:22:17 -0500

    working through guide to get familiar with go

--- go-cli.go
+++ go-cli.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	flag "github.com/ogier/pflag"
+)
+
+var (
+	user  string
+	users []string
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	// parse flags
+	flag.Parse()
+
+	// if user does not supply flags, print usage
+	// we can clean this up later by putting this into its own function
+	if flag.NFlag() == 0 {
+		fmt.Printf("Usage: %s [options]\n", os.Args[0])
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	users = strings.Split(user, ",")
+	fmt.Printf("Searching user(s): %s\n", users)
+}
+
+func init() {
+	flag.StringVarP(&user, "user", "u", "", "Search Users")
 }
